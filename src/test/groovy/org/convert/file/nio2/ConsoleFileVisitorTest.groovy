dd6c79fec6dd5303ae31d3f394b42f48e84bf9e1
JamesChang <jam>
2021-01-16 16:40:53 +0800

    test: 遞迴走訪資料夾內容

--- src/test/groovy/org/convert/file/nio2/ConsoleFileVisitorTest.groovy
+++ src/test/groovy/org/convert/file/nio2/ConsoleFileVisitorTest.groovy
@@ -5,8 +5,19 @@ import java.nio.file.Path
 import java.nio.file.SimpleFileVisitor
 import java.nio.file.attribute.BasicFileAttributes
 
+/**
+ * 實作FileVisitorTest範例
+ * 目的：列出資料夾裏面所有檔案內容
+ */
 class ConsoleFileVisitorTest extends SimpleFileVisitor<Path>{
 
+    /**
+     * 列出資料夾
+     * @param path
+     * @param atters
+     * @return
+     * @throws IOException
+     */
     @Override
     FileVisitResult preVisitDirectory(
             Path path,
@@ -14,10 +25,17 @@ class ConsoleFileVisitorTest extends SimpleFileVisitor<Path>{
     )
     throws IOException
     {
-        println path.getFileName()
+        println "show Directory before : "+path.getFileName()
         return FileVisitResult.CONTINUE
     }
 
+    /**
+     * 列出檔案
+     * @param path
+     * @param atters
+     * @return
+     * @throws IOException
+     */
     @Override
     FileVisitResult visitFile(
             Path path,
@@ -25,17 +43,33 @@ class ConsoleFileVisitorTest extends SimpleFileVisitor<Path>{
     )
             throws IOException
     {
-        println path.getFileName()
+        println "is File : "+path.getFileName()
         return FileVisitResult.CONTINUE
     }
 
+    /**
+     * 讀取失敗
+     * @param path
+     * @param ex
+     * @return
+     */
     @Override
     FileVisitResult visitFileFailed(
             Path path,
             IOException ex
     )
     {
+        println "has error in File : "+path.getFileName()
         ex.printStackTrace()
+        return FileVisitResult.CONTINUE
+    }
+
+    FileVisitResult postVisitDirectory(
+            Path path, IOException ex
+    )
+    throws IOException
+    {
+        println "show Directory after : "+path.getFileName()
         return FileVisitResult.CONTINUE
     }
 }
